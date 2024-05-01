dbfc01fbe91b65bf0f0e033a6201c3f0d229512e
ZBlux <aminekamili@hotmail.com>
2024-04-30 18:05:01 -0700

    insertion

--- sort.h
+++ sort.h
@@ -27,7 +27,7 @@ void print_list(const listint_t *list);
 /* sorting algorithms prototypes */
 
 void bubble_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
 
 
 #endif /* SORT_H */
